e97743ed200548d65a39894aa585910f79444e7b
Jida Li <77774296+jidalii@users.noreply.github.com>
2024-04-06 22:46:19 -0400

    setup

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { API_URL_KEY, PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  defaultNetwork: "mumbai",
+  networks: {
+    hardhat: {},
+    mumbai: {
+      url: API_URL_KEY,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
