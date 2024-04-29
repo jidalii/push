d0eaba3d319c88918802f6eeda5779474b8371bc
Jida Li <77774296+jidalii@users.noreply.github.com>
2024-04-29 15:13:39 -0400

    fix: solved zk-proof gen issue

--- server/routers/zk_proof.js
+++ server/routers/zk_proof.js
@@ -18,6 +18,7 @@ router.get("/gen/running", async (req, res) => {
     minPace: body.minPace,
     minDistance: body.minDistance,
   };
+  
   try {
     const { proof, publicSignals } = await snarkjs.plonk.fullProve(
       input,
@@ -25,19 +26,26 @@ router.get("/gen/running", async (req, res) => {
       "./circom2contract/running/circuit_final.zkey"
     );
 
-    const proofArrObj = reorderDict(proof);
+    const vKey = JSON.parse(
+      fs.readFileSync("./circom2contract/running/verification_key.json")
+    );
+    const result = await snarkjs.plonk.verify(vKey, publicSignals, proof);
+    console.log("Verification: ", result);
 
-    const tempProof = convertProofToHex(proofArrObj)
-    const formattedProof = convertObjectToHexPadded(tempProof);
-    const pubSignalHex = convertToHexPadded(publicSignals);
+    const calldataBlob = await snarkjs.plonk.exportSolidityCallData(
+      proof,
+      publicSignals
+    );
+    const calldata = calldataBlob.split("][");
 
-    console.log("length:", formattedProof.length);
+    let proofHex = JSON.parse(calldata[0] + "]");
+    let signalsHex = JSON.parse("[" + calldata[1]);
 
     res.send({
       proof: proof,
-      proofHex: formattedProof,
+      proofHex: proofHex,
       publicSignals: publicSignals,
-      publicSignalsHex: pubSignalHex,
+      publicSignalsHex: signalsHex,
     });
   } catch (error) {
     res.status(400).send(error.message);
@@ -62,123 +70,31 @@ router.get("/gen/sleeping", async (req, res) => {
       "./circom2contract/sleeping/sleeping_js/sleeping.wasm",
       "./circom2contract/sleeping/circuit_final.zkey"
     );
-    const proofArrObj = reorderDict(proof);
 
-    const tempProof = convertProofToHex(proofArrObj)
-    const formattedProof = convertObjectToHexPadded(tempProof);
-    const pubSignalHex = convertToHexPadded(publicSignals);
+    const vKey = JSON.parse(
+      fs.readFileSync("./circom2contract/sleeping/verification_key.json")
+    );
+    const result = await snarkjs.plonk.verify(vKey, publicSignals, proof);
+    console.log("Verification: ", result);
 
-    console.log("length:", formattedProof.length);
+    const calldataBlob = await snarkjs.plonk.exportSolidityCallData(
+      proof,
+      publicSignals
+    );
+    const calldata = calldataBlob.split("][");
+
+    let proofHex = JSON.parse(calldata[0] + "]");
+    let signalsHex = JSON.parse("[" + calldata[1]);
 
     res.send({
       proof: proof,
-      proofHex: formattedProof,
+      proofHex: proofHex,
       publicSignals: publicSignals,
-      publicSignalsHex: pubSignalHex,
+      publicSignalsHex: signalsHex,
     });
   } catch (error) {
     res.status(400).send(error.message);
   }
 });
 
-function convertObjectToHexPadded(obj) {
-  const hexArray = [];
-
-  // Function to convert a single BigInt number to a padded hex string
-  const toPaddedHexString = (bigInt) => {
-    return "0x" + bigInt.toString(16).padStart(64, "0");
-  };
-
-  // Iterate over each key in the object
-  for (const key of Object.keys(obj)) {
-    const value = obj[key];
-
-    // Check if the value is an array (handle nested arrays too)
-    if (Array.isArray(value)) {
-      // Handle nested arrays for points like A, B, C, etc.
-      if (Array.isArray(value[0])) {
-        value.forEach((innerArray) => {
-          innerArray.forEach((number) => {
-            hexArray.push(toPaddedHexString(BigInt(number)));
-          });
-        });
-      } else {
-        // Handle flat arrays for points like eval_a, eval_b, etc.
-        value.forEach((number) => {
-          hexArray.push(toPaddedHexString(BigInt(number)));
-        });
-      }
-    } else {
-      // Handle single values that are not arrays
-      hexArray.push(toPaddedHexString(BigInt(value)));
-    }
-  }
-
-  return hexArray;
-}
-
-function convertProofToHex(proof) {
-  // Helper function to convert to a 0x-prefixed hexadecimal string
-  const toHex = (numStr) => {
-    return "0x" + BigInt(numStr).toString(16);
-  };
-
-  // Convert the proof object, excluding the "1"s and non-numeric properties
-  let convertedProof = {};
-  for (let [key, value] of Object.entries(proof)) {
-    if (key !== "protocol" && key !== "curve") {
-      if (key.startsWith("eval_")) {
-        // Convert single numeric strings directly
-        convertedProof[key] = toHex(value);
-      } else {
-        // Convert arrays, excluding the last element "1"
-        convertedProof[key] = value.slice(0, -1).map(toHex);
-      }
-    }
-  }
-
-  return convertedProof;
-}
-
-function reorderDict(dict) {
-  dictReorder = [
-    { key: "A", value: dict["A"] },
-    { key: "B", value: dict["B"] },
-    { key: "C", value: dict["C"] },
-    { key: "Z", value: dict["Z"] },
-    { key: "T1", value: dict["T1"] },
-    { key: "T2", value: dict["T2"] },
-    { key: "T3", value: dict["T3"] },
-    { key: "Wxi", value: dict["Wxi"] },
-    { key: "Wxiw", value: dict["Wxiw"] },
-    { key: "eval_a", value: dict["eval_a"] },
-    { key: "eval_b", value: dict["eval_b"] },
-    { key: "eval_c", value: dict["eval_c"] },
-    { key: "eval_s1", value: dict["eval_s1"] },
-    { key: "eval_s2", value: dict["eval_s2"] },
-    { key: "eval_zw", value: dict["eval_zw"] },
-  ];
-  result = {}
-  for (let item of dictReorder) {
-    result[item.key] = item.value;
-  }
-
-  return result;
-}
-
-function convertToHexPadded(pubSignals) {
-  return pubSignals.map((signal) => {
-    // Convert the decimal string to a BigInt
-    const bigIntValue = BigInt(signal);
-
-    // Convert BigInt to a hexadecimal string
-    let hexValue = bigIntValue.toString(16);
-
-    // Pad the hexadecimal string with leading zeros to ensure 64 characters
-    // 64 characters correspond to 32 bytes or 256 bits, which is typical for Ethereum
-    const paddedHex = "0x" + hexValue.padStart(64, "0");
-
-    return paddedHex;
-  });
-}
 module.exports = router;
